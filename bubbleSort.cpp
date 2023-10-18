a86d99f7b27756dbe737fc314940a5fe642dab62
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-10-18 15:38:05 +0300

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -7,6 +7,6 @@ int main()
         int arr = {23, 34,12, 19, 45, 98, 45,98, 100,13, 76,8};
         for (int i = 1;i < 12; i++)
         {
-                for(int j = 1; j < 17; j++)
+                for(int j = 0; j < 17; j++)
         }
 }
