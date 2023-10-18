02fc20221edd129ac40c0aa268926e52c7c212c3
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-10-18 15:39:44 +0300

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -10,6 +10,9 @@ int main()
                 for(int j = 0; j < 17; j++)
                 {
                         if (arr[j] > arr[j+1])
+                        {
+                                
+                        }
                 }
         }
 }
