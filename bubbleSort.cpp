a104acaee268e816064ab94f9365d7e39a3995be
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-10-18 15:41:22 +0300

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -13,6 +13,7 @@ int main()
                         {
                                 int temp = arr[j];
                                 arr[j] = arr[j+1];
+                                arr[j+1] = temp;
                         }
                 }
         }
