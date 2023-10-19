21a39cddfb0baaa82d108490a11b74f2eae27a2e
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-10-19 21:04:32 +0300

    Update insertMiddle.cpp

--- insertMiddle.cpp
+++ insertMiddle.cpp
@@ -13,6 +13,7 @@ class Node
         }
         void printList()
         {
-                Node *temp = this;      
+                Node *temp = this;
+                while( temp ! = NULL)      
         }
 }
