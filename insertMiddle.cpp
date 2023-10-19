901ef09c1eeb64883a48d177fa674801e8314fbb
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-10-19 21:06:05 +0300

    Update insertMiddle.cpp

--- insertMiddle.cpp
+++ insertMiddle.cpp
@@ -17,6 +17,7 @@ class Node
                 while( temp ! = NULL)  
                 {
                         cout<<temp->data<<"";
+                        temp = temp->next;
                 }    
         }
 }
