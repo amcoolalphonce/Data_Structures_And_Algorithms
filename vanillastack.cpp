b041a8b3ed59e47f090871f37312277920d2fc6f
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-10-28 18:56:25 +0300

    Update vanillastack.cpp

--- vanillastack.cpp
+++ vanillastack.cpp
@@ -134,9 +134,9 @@ int main()
         cout<<"A peek at the top of the stack reveals: "<<vanillaStack.peek()<<endl;//7 expected
 
         vanillaStack.pop();
-        vanillaStack.printStack();// 5 3  8 6
+        vanillaStack.printStack();// 5 3  8  6
         vanillaStack.pop();
-        vanillaStack.printStack(); // 5 3 
+        vanillaStack.printStack(); // 5 3  8
         vanillaStack.pop();
-        vanillaStack.printStack();
+        vanillaStack.printStack();// 5 3 
 }
